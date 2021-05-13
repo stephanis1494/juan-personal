a46e7c9bcee7c8101fe2148024cc08b2ea4d6775
unknown <juanluischaurant@gmail.com>
2021-05-13 11:27:06 -0400

    improved (or not) the html layout of the navbar

--- src/components/presentations/Navbar.jsx
+++ src/components/presentations/Navbar.jsx
@@ -1,15 +1,17 @@
 const Navbar = () => {
     return ( 
-        <nav className="navbar">
-            <h1>Juan's Blog</h1>
-            <div className="links">
-                <a href="/">home</a>
-                <a href="/blog">blog</a>
-                <a href="/projects">projects</a>
-                <a href="/play">play</a>
-                <a href="tools">tools</a>
-            </div>
-        </nav>
+        <div className="navbar-container">
+            <nav className="navbar">
+                <h1>Juan's Blog</h1>
+                <div className="links">
+                    <a href="/">home</a>
+                    <a href="/blog">blog</a>
+                    <a href="/projects">projects</a>
+                    <a href="/play">play</a>
+                    <a href="tools">tools</a>
+                </div>
+            </nav>
+        </div>
     );
 }
  
