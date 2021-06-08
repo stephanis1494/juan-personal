7c0980e57248b1ff1affd88797407a426fb5cfa5
unknown <juanluischaurant@gmail.com>
2021-06-07 22:54:54 -0400

    added a card component and npm framer-motion

--- src/components/views/Projects.jsx
+++ src/components/views/Projects.jsx
@@ -1,28 +1,42 @@
 import ContentHero from "../presentations/ContentHero"
 
+
+export const ListItem = ({link="https://res.cloudinary.com/df0ll615k/image/upload/v1622512511/about_us_page_background.jpg", header="This is a header", desc="This is a description"}) => {
+    return (
+        <div className="card">
+
+            <div className="card_image">
+                <img src={link} />
+            </div>
+
+            <div className="card_content">
+                <h2 className="card_heading">{header}</h2>
+                <p className="card_description">{desc}</p>
+                <button className="card_button">Go to project</button>
+            </div>
+
+        </div>
+
+    );
+}
+
+
 const Projects = () => {
     return (
         <>
             <ContentHero title="Projects" />
-            <ul class="cards">
+            <div className="cards">
 
-  	            <li class="cards_item">
-                    <div class="card">
-            
-                        <div class="card_image">
-                                <img src="https://res.cloudinary.com/df0ll615k/image/upload/v1622512511/about_us_page_background.jpg" />
-                        </div>
-            
-                        <div class="card_content">
-                                <h2 class="card_heading">First</h2>
-                                <p>I'm a card and I'm first. Quisque commodo hendrerit lorem quis egestas. Maecenas quis tortor arcu. Vivamus rutrum nunc non neque consectetur quis placerat neque.</p>
-                                <a href="#" class="card_button">Button</a>
-                        </div>
-            
-                    </div>
-	            </li>
-                
-            </ul>
+                {/* card 1 */}
+  	            <ListItem />
+
+                {/* card 2 */}
+  	            <ListItem />
+
+                {/* card 3 */}
+  	            <ListItem />
+
+            </div>
         </>
     );
 }
