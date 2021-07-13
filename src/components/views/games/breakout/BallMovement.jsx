d76e42b73e8291ed3a3091ffd4af4773c8f1931f
Stephen Fairris <stephen@stelsey.com>
2021-07-12 19:24:09 -0700

    Move Ball declaration to outside of function to invoke movement

--- src/components/views/games/breakout/BallMovement.jsx
+++ src/components/views/games/breakout/BallMovement.jsx
@@ -1,11 +1,3 @@
-export function BallMovement(TheContext, ballObject) {
-    let data = new Ball(ballObject.x, ballObject.y, ballObject.rad)
-    data.draw(TheContext)
-    ballObject.x += 8
-    ballObject.x += ballObject.dx
-    // ballObject.y += ballObject.dy
-}
-
 class Ball {
     constructor(x, y, rad) {
         this.x = x;
@@ -15,13 +7,21 @@ class Ball {
 
     draw(TheContext) {
         TheContext.beginPath()
-            
+
         TheContext.fillStyle = 'red'
         TheContext.arc(this.x, this.y, this.rad, 0, 2*Math.PI)
         TheContext.strokeStyle = 'black'
         TheContext.strokeWidth = 4
-        
+
         TheContext.fill()
         TheContext.stroke()
+
+        this.x++
     }
+}
+
+let data = new Ball(0, 100, 10)
+
+export function BallMovement(TheContext, ballObject) {
+    data.draw(TheContext)
 }
