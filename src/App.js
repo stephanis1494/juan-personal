dda934f98e8a5e29352315ed17993948d1520801
Juan Luis Chaurant <juanluischaurant@gmail.com>
2021-12-30 14:17:23 -0400

    indented some elements at App.js

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ import { store } from './store'
 
 const App = () => {
   return (
-        <BrowserRouter>
-          <div className="content">
-            <Provider store={store}>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/blog' component={Blog} />
-              <Route path='/projects' component={Projects} />
-              <Route path='/play' component={Play} />
-              <Route path='/tools' component={Tools} />
-              <Route path='/about' component={About} />
-              <Route path='/qr-code' component={QRCode} />
-              <Route path='/games/breakout' component={Breakout} />
-              <Route path='/games/flappysquare' component={FlappySquare} />
-              <Route path='/music' component={Music} />
-            </Switch>
-            </Provider>
-          </div>
-          {/* <Footer /> */}
-        </BrowserRouter>
+    <BrowserRouter>
+      <div className="content">
+        <Provider store={store}>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/blog' component={Blog} />
+          <Route path='/projects' component={Projects} />
+          <Route path='/play' component={Play} />
+          <Route path='/tools' component={Tools} />
+          <Route path='/about' component={About} />
+          <Route path='/qr-code' component={QRCode} />
+          <Route path='/games/breakout' component={Breakout} />
+          <Route path='/games/flappysquare' component={FlappySquare} />
+          <Route path='/music' component={Music} />
+        </Switch>
+        </Provider>
+      </div>
+      {/* <Footer /> */}
+    </BrowserRouter>
   )
 }
 
